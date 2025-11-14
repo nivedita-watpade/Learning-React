d46e5ba37fc4d4f9c1db2a7d3b475f154778ae00
nivedita-watpade <niveditawatpade298@gmail.com>
2025-11-14 13:01:45 +0530

    implementd toggle functionality on close icon

--- 04-steps/starter/src/App.jsx
+++ 04-steps/starter/src/App.jsx
@@ -9,6 +9,8 @@ const messages = [
 function App() {
   // const step = 2;
   const [step, setStep] = useState(1);
+  const [open, setOpen] = useState(true);
+
   function handlePrevious() {
     if (step > 1) {
       setStep(step - 1);
@@ -17,31 +19,41 @@ function App() {
   function handleNext() {
     if (step < 3) setStep(step + 1);
   }
+  function displayStepper() {
+    setOpen(!open);
+  }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 && "active"}>1</div>
-        <div className={step >= 2 && "active"}>2</div>
-        <div className={step >= 3 && "active"}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={displayStepper}>
+        &times;
+      </button>
+      {open && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 && "active"}>1</div>
+            <div className={step >= 2 && "active"}>2</div>
+            <div className={step >= 3 && "active"}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
