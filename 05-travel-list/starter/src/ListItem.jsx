cdd6b9796779a7a62d550f973ddf967782619a15
nivedita-watpade <niveditawatpade298@gmail.com>
2025-12-11 12:48:11 +0530

    updating items

--- 05-travel-list/starter/src/ListItem.jsx
+++ 05-travel-list/starter/src/ListItem.jsx
@@ -8,10 +8,26 @@ function ListItem(props) {
       })
     );
   }
+
+  function handleCheckList(itemId) {
+    setItems((prevItems) =>
+      prevItems.map((prevItem) => {
+        if (prevItem.id === itemId) {
+          return { ...prevItem, packed: !prevItem.packed };
+        } else {
+          return prevItem;
+        }
+      })
+    );
+  }
+
   return (
     <li>
-      <input type="checkbox" />
-
+      <input
+        type="checkbox"
+        // value={item.packed}
+        onChange={() => handleCheckList(item.id)}
+      />
       <span
         className={item.packed ? "strike" : " "}
         //  style={item.packed ? { textDecoration: "line-through" } : {}}
