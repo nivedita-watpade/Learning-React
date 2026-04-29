961008001d85a09228560008be31322e2236d94f
nivedita-watpade <niveditawatpade298@gmail.com>
2026-04-29 10:40:32 +0530

    React redux

--- 15-redux-intro/redux-intro/src/App.jsx
+++ 15-redux-intro/redux-intro/src/App.jsx
@@ -2,16 +2,24 @@ import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers/Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
-import store from "./store";
+import { useSelector } from "react-redux";
 
 function App() {
+  const customer = useSelector((store) => store.customer.fullName);
+  console.log(customer);
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+
+      {!customer ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
--- 15-redux-intro/redux-intro/src/features/accounts/BalanceDisplay.jsx
+++ 15-redux-intro/redux-intro/src/features/accounts/BalanceDisplay.jsx
@@ -1,3 +1,5 @@
+import { connect, useSelector } from "react-redux";
+
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
@@ -5,8 +7,17 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+  // const balance = useSelector((state) => state.account.balance);
+
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+//Old Way
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
